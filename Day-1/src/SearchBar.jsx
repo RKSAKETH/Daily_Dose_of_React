6a7bdd3c9821a1e917cdb0f58fcf0821ba29b116
RKSAKETH <krishnasaketh566@gmail.com>
2025-12-23 23:59:22 +0530

    added debounce timer

--- Day-1/src/SearchBar.jsx
+++ Day-1/src/SearchBar.jsx
@@ -11,23 +11,24 @@ export default function SearchBar(){
             return;
         }
 
-        fetchSuggestions(query);
+        const debounceTimer = setTimeout(()=>{
+            fetchSuggestions(query);
+        },300)
 
+        return () => clearTimeout(debounceTimer);
     },[query]);
 
     const fetchSuggestions = async (searchText) => {
+        setLoading(true);
         try{
             const data = await fetch(`https://api.datamuse.com/sug?s=${searchText}`);
             const response = await data.json();
             setSuggestions(response);
-        }
-        catch(error){
+        }catch(error){
             console.log("Error: ",error)
+        }finally{
+            setLoading(false);
         }
-        finally{
-        }
-        
-        
     };
 
     return(
